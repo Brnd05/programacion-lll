92089979a7bc7537fb000a9d8f18b61841ecca6e
Brnd05 <grisa9547@gmail.com>
2025-08-19 13:18:53 -0600

    Agregando MetodoGenerico como carpeta normal

---
+++ Semana 2/MetodoGenerico/MetodoGenerico/Program.cs
@@ -0,0 +1,38 @@
+﻿//Echo
+//Hola
+using MetodoGenerico;
+var echo1 = UtileriaGenerica.Echo(35);
+var echo2 = UtileriaGenerica.Echo("Programacion III");
+
+Console.WriteLine($"Echo int: {echo1}");
+Console.WriteLine($"Echo string: {echo2}");
+
+//EsIgual
+Console.WriteLine($"5 es igual que 5: {UtileriaGenerica.EsIgual(5,5)}");
+Console.WriteLine($"a es igual que b: {UtileriaGenerica.EsIgual("a", "b")}");
+
+//Max
+Console.WriteLine($"Maximo entre 7 y 5: {UtileriaGenerica.Max(5,7)}");
+
+//Repetir un valor varias veces 
+var repetirEnteros = UtileriaGenerica.Repetir(10,6);
+Console.WriteLine($"Repeticio de Enteros: {string.Join(",", repetirEnteros)}");
+Console.WriteLine("=========================================================");
+
+void MostrarInformación<T>(T valor) { 
+    Console.WriteLine($"Valor recibido: {valor}");
+}
+
+MostrarInformación<int>(50);
+MostrarInformación<string>("Hola mundo");
+//Operador ternario
+// condición  ? valorSiEsVeradero : valorSiEsFalso
+
+int x = 3;
+string resultado = x > 0 ? "Positivo" : "Negativo";
+
+if (x > 0)
+    resultado = "Positivo";
+else
+    resultado = "Negativo";
+    Console.ReadLine();
---
+++ Semana 2/MetodoGenerico/MetodoGenerico/UtileriaGenerica.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MetodoGenerico
+{
+    public static class UtileriaGenerica
+    {
+        //Devuelve lo que recibe 
+        public static T Echo<T>(T valor)
+        {
+            return valor;
+        }
+
+        //igualdad generica para cualquier tipo
+        public static bool EsIgual<T>(T primerValor, T segundoValor)
+        {
+            return EqualityComparer<T>.Default.Equals(primerValor, segundoValor);
+        }
+
+        public static T Max<T>(T primerValor, T segundoValor) where T : IComparable<T>
+        {
+            return primerValor.CompareTo(segundoValor)
+                >= 0 ? primerValor : segundoValor;
+        }
+
+        //Repetir un valor varias veces, y devuelve una lista
+
+        public static List<T> Repetir<T>(T valor, int repetir)
+        {
+            var Lista = new List<T>();
+            for (int i = 0; i < repetir; i++)
+                Lista .Add(valor);
+
+            return Lista;
+
+        }   
+    }
+}
