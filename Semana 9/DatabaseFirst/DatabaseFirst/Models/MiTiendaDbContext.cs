94007f9d213bad6186d4a599109fb6645e1aae5c
Brnd05 <grisa9547@gmail.com>
2025-10-07 07:33:08 -0600

    Se avanzo en CodeFirst

---
+++ Semana 9/DatabaseFirst/DatabaseFirst/Models/MiTiendaDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirst.Models;
+
+public partial class MiTiendaDbContext : DbContext
+{
+    public MiTiendaDbContext()
+    {
+    }
+
+    public MiTiendaDbContext(DbContextOptions<MiTiendaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categorias { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server =DESKTOP-9C7BQPH\\SQLEXPRESS; Database=MiTiendaDb; User Id=sa; Password=1234; Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Categori__3214EC07EF44061F");
+
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC0723A9BF4A");
+
+            entity.Property(e => e.Codigo).HasMaxLength(50);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Categoria).WithMany(p => p.Productos)
+                .HasForeignKey(d => d.CategoriaId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Productos_Categorias");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Semana 9/DatabaseFirst/DatabaseFirst/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirst.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public decimal Precio { get; set; }
+
+    public int CategoriaId { get; set; }
+
+    public string? Codigo { get; set; }
+
+    public virtual Categoria Categoria { get; set; } = null!;
+}
