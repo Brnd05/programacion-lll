50770b093382d8b13bbb5eccfb44af657df361b5
Brnd05 <grisa9547@gmail.com>
2025-11-20 11:40:31 -0600

    se agrego la semana 17

---
+++ Semana 17/UMarket/UMarket.Domain/Entities/Venta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMarket.Domain.Entities
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Total { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+    }
+}
---
+++ Semana 17/UMarket/UMarket.Domain/Entities/VentaDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMarket.Domain.Entities
+{
+     public class VentaDetalle
+    {
+        public int Id { get; set; }
+        public int VentaId { get; set; }
+        public Venta Venta { get; set; }
+        public int ProductoId { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal SubTotal => Cantidad * PrecioUnitario;
+    }
+}
